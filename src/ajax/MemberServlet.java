b04ddcf9ead8935c652cb86b6d05987c75d329d1
admin <admin@YD02-13>
2021-04-29 13:53:20 +0900

    210429 13:53

--- src/ajax/MemberServlet.java
+++ src/ajax/MemberServlet.java
@@ -3,6 +3,7 @@ package ajax;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.servlet.ServletException;
@@ -13,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import common.DBCon;
 
-@WebServlet("/jquery/memberServlet.do")	// 실제로 있는 건 아니고, 가상의 주소 같은 느낌. do도 되고 html도 되고, 대체 가능.
+@WebServlet("/jquery/memberServlet.do") // 실제로 있는 건 아니고, 가상의 주소 같은 느낌. do도 되고 html도 되고, 대체 가능.
 public class MemberServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -28,19 +29,69 @@ public class MemberServlet extends HttpServlet {
 		// doGet : 조회를 위함. 디폴트 방식
 		response.setContentType("text/html;charset=UTF-8");
 //		response.getWriter().print("<h2>정상 조회 완료</h2>");
-		
+
 		/*
-			조회 sql을 작성
+		 * 조회 sql을 작성
+		 * 
+		 * 조회 결과를 json 형식으로 작성 [{"id:1, "name": "hong", "age" : 20},
+		 * {"id:2, "name": "hang", "age" : 21}, {"id:3, "name": "hing", "age" : 22}]
+		 */
+
+		Connection conn = DBCon.getConnect();
+		PreparedStatement psmt = null;
+		ResultSet rs = null;
+		String sql = "select member_id, member_name, member_age from member";
+		String json = "[";
+		try {
+			psmt = conn.prepareStatement(sql,
+										ResultSet.TYPE_SCROLL_INSENSITIVE,
+										ResultSet.CONCUR_UPDATABLE);
+			rs = psmt.executeQuery();
+			while (rs.next()) {
+				String memId = rs.getString(1);
+				String memName = rs.getString(2);
+				String memAge = rs.getString(3); // 굳이 int로 안 가져와도 됨.
+
+				json += "{\"id\":\"" + memId + "\","
+						+ "\"name\":\"" + memName + "\"," 
+						+ "\"age\":\"" + memAge + "\"}";
+				if (!rs.isLast())
+					json += ",";
+			}
 			
-			조회 결과를 json 형식으로 작성
-			[{"id:1, "name": "hong", "age" : 20},
-			{"id:2, "name": "hang", "age" : 21},
-			{"id:3, "name": "hing", "age" : 22}]
-		*/
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+
+			if (psmt != null) {
+				try {
+					psmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			
+		}
+
+		json += "]";
 		
-		
-		
-		
+		response.getWriter().print(json);
+		// 결과를 response() 메소드를 이용해 출력
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -85,6 +136,8 @@ public class MemberServlet extends HttpServlet {
 			}
 		}
 
+		// {"id":1, "name":"hong", "age":20}
+		String json = "{\"id\":"+ p1 +",\"name\":\"" + p2 + "\",\"age\":" + p3 + "}";
 		response.getWriter().print("<h2>정상 입력</h2>");
 
 	}
