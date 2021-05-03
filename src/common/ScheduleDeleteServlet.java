928466b5322c53694eb97d2daf11d4ec15f054db
pts1006 <pts1006@example.com>
2021-05-03 19:43:18 +0900

    210503 19:43

--- src/common/ScheduleDeleteServlet.java
+++ src/common/ScheduleDeleteServlet.java
@@ -8,10 +8,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/deleteScheduleServlet")
-public class DeleteScheduleServlet extends HttpServlet {
+public class ScheduleDeleteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	public DeleteScheduleServlet() {
+	public ScheduleDeleteServlet() {
 		super();
 
 	}
